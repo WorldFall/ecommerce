23619937985a4c6738452465ed3561f4b9f4cc7d
Alexandre <alexandresofiati@hotmail.com>
2018-08-16 22:39:00 -0300

    Criando a classe page

---
+++ vendor/hcodebr/php-classes/src/Page.php
@@ -0,0 +1,51 @@
+<?php 
+	namespace Hcode;
+
+	use \Rain\Tpl;
+
+class Page{
+	private $tpl;
+	private $options = [];
+	private $defaults = ["data"=>[]];
+
+	public function __construct($opts = array()){
+
+		$this->options = array_merge($this->defaults, $opts);
+
+		//Configuração
+		$config = array(
+			"tpl_dir" => $_SERVER["DOCUMENT_ROOT"]."/views/",
+			"cache_dir" => $_SERVER["DOCUMENT_ROOT"]."views-cache/",
+			"debug" => false
+		);
+
+		Tpl::configure($config);
+
+		$this->tpl = new Tpl;
+
+		$this->setData($this->options["data"]);
+
+		$this->tpl->draw("header");
+	}
+
+	private function setData($data = array()){
+		foreach($data as $key => $value){
+			$this->tpl->assign($key, $value);
+		}
+	}
+
+	//renderizar o html
+	public function setTpl($name, $data = array(), $returnHTML = false){
+		$this->setData($data);
+
+		$this->tpl->draw($name, $returnHTML);
+	}
+
+	//chamar o fotter
+	public function __destruct(){
+
+		$this->tpl->draw("footer");
+
+	}
+}
+?>
