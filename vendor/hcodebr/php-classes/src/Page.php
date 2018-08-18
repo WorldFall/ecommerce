770c66458519783059d9902c43488dee081ed91f
Alexandre <alexandresofiati@hotmail.com>
2018-08-18 20:56:44 -0300

    tela de login

--- vendor/hcodebr/php-classes/src/Page.php
+++ vendor/hcodebr/php-classes/src/Page.php
@@ -6,7 +6,11 @@
 class Page{
 	private $tpl;
 	private $options = [];
-	private $defaults = ["data"=>[]];
+	private $defaults = [
+		"header"=>true,
+		"footer"=>true,
+		"data"=>[]
+	];
 
 	public function __construct($opts = array(), $tpl_dir = "/views/"){
 
@@ -25,7 +29,7 @@ class Page{
 
 		$this->setData($this->options["data"]);
 
-		$this->tpl->draw("header");
+		if($this->options["header"] === true)$this->tpl->draw("header");
 	}
 
 	private function setData($data = array()){
@@ -44,7 +48,7 @@ class Page{
 	//chamar o fotter
 	public function __destruct(){
 
-		$this->tpl->draw("footer");
+		if($this->options["footer"] === true) $this->tpl->draw("footer");
 
 	}
 }
